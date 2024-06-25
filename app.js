59934ae0266f127d34f8c85f7c80340f68b49a82
daren <darenhein@hotmail.com>
2024-06-25 14:42:47 -0600

    agrenado dependicnas

--- app.js
+++ app.js
@@ -50,13 +50,13 @@ app.get('/login' , (req,res) => {
 
 })
 
-// ahora recibiremos datos desde inico.html 
-app.get('/ingreso/:correo/:contra', (req,res) => {
 
-	var correo = req.params.correo
+
+app.use("/ingreso/:correo/:contra",(req,res,next) => {
+
+	console.log("Solicitud Entrate")
 	var contra = req.params.contra
-	console.log(correo)
-	console.log(contra)
+	var correo = req.params.correo
 	const consulta = "select email from coordinador where email = ? and contra = ? "
 	conexion.query(consulta , [correo,contra] , (error,resultado) => {
 		// 
@@ -65,9 +65,23 @@ app.get('/ingreso/:correo/:contra', (req,res) => {
 		}else {
 			res.json(resultado)
 			console.log("Exito")
+			next()
 		}
 	})
+	
 })
+
+
+// ahora recibiremos datos desde inico.html 
+app.get('/ingreso/:correo/:contra', (req,res) => {
+
+	var correo = req.params.correo
+	var contra = req.params.contra
+	//console.log(correo)
+	//console.log(contra)
+	
+})
+ 
 
 // ruta de busqueda por alumno de manera individual 
 
